0968f9412b1864845b6f2e4ecfadf20aac2cd5da
JDemlow <jdemlowdev@gmail.com>
2024-07-02 04:11:09 -0600

    added building cards back

--- src/components/BuildingList.jsx
+++ src/components/BuildingList.jsx
@@ -4,6 +4,7 @@ function BuildingsList() {
   const [buildings, setBuildings] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [buildingsPerPage] = useState(10); // Number of buildings per page
+  const [expandedAddresses, setExpandedAddresses] = useState({});
 
   useEffect(() => {
     const fetchBuildings = async () => {
@@ -31,69 +32,106 @@ function BuildingsList() {
     setCurrentPage(currentPage - 1);
   };
 
+  const toggleAddress = (id) => {
+    setExpandedAddresses((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  };
+
   return (
-    <div className="flex flex-col items-center pt-8">
-      <h2 className="text-2xl font-bold mb-4">Buildings List</h2>
+    <div className="min-h-screen p-6 bg-gray-100">
+      <h1 className="mb-6 text-3xl font-bold text-center">Buildings List</h1>
       {Array.isArray(buildings) ? (
-        <ul className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
           {buildings.map((building) => (
-            <li
+            <div
               key={building._id}
-              className="bg-white shadow-md rounded-lg p-6 mb-4"
+              className="relative bg-white shadow-md rounded-xl"
             >
-              <div className="font-bold text-xl mb-2">
-                Building ID: {building.buildingId}
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="my-2 text-gray-600">
+                    Building ID: {building.buildingId}
+                  </div>
+                  <h3
+                    className="text-xl font-bold cursor-pointer"
+                    onClick={() => toggleAddress(building._id)}
+                  >
+                    Street Address:{" "}
+                    <span>
+                      {expandedAddresses[building._id]
+                        ? building.streetAddress
+                        : `${building.streetAddress.slice(0, 12)}`}
+                      {!expandedAddresses[building._id] &&
+                        building.streetAddress.length > 12 && (
+                          <span className="text-gray-400">...</span>
+                        )}
+                    </span>
+                  </h3>
+                </div>
+                <div className="mb-5">
+                  Building Size:{" "}
+                  {building.buildingSize || "No size information"}
+                </div>
+                <div className="mb-5">
+                  Property Use 1st: {building.propertyUse1st || "N/A"}
+                </div>
+                <div className="mb-5">
+                  Property Use 2nd: {building.propertyUse2nd || "N/A"}
+                </div>
+                <div className="mb-5">
+                  Property Use 3rd: {building.propertyUse3rd || "N/A"}
+                </div>
+                <div className="mb-5">
+                  Benchmarking Status: {building.benchmarkingStatus || "N/A"}
+                </div>
+                <div className="mb-5">
+                  Current Site EUI: {building.currentSiteEUI || "N/A"}
+                </div>
+                <div className="mb-5">
+                  Baseline 2019 EUI: {building.baseline2019EUI || "N/A"}
+                </div>
+                <div className="mb-5">
+                  1st Target 2025 EUI: {building.firstTarget2025EUI || "N/A"}
+                </div>
+                <div className="mb-5">
+                  2nd Target 2027 EUI: {building.secondTarget2027EUI || "N/A"}
+                </div>
+                <div className="mb-5">
+                  Final Target 2030 EUI: {building.finalTarget2030EUI || "N/A"}
+                </div>
+                <div className="flex justify-left">
+                  <button className="px-6 py-3 mt-4 text-lg text-white rounded-lg bg-emerald-500 hover:bg-emerald-600">
+                    Show Building
+                  </button>
+                </div>
               </div>
-              <p className="text-gray-700 text-base">
-                Street Address: {building.streetAddress}
-              </p>
-              <p className="text-gray-700 text-base">
-                Building Size: {building.buildingSize || "No size information"}
-              </p>
-              <p className="text-gray-700 text-base">
-                Property Use 1st: {building.propertyUse1st || "N/A"}
-              </p>
-              <p className="text-gray-700 text-base">
-                Property Use 2nd: {building.propertyUse2nd || "N/A"}
-              </p>
-              <p className="text-gray-700 text-base">
-                Property Use 3rd: {building.propertyUse3rd || "N/A"}
-              </p>
-              <p className="text-gray-700 text-base">
-                Benchmarking Status: {building.benchmarkingStatus || "N/A"}
-              </p>
-              <p className="text-gray-700 text-base">
-                Current Site EUI: {building.currentSiteEUI || "N/A"}
-              </p>
-              <p className="text-gray-700 text-base">
-                Baseline 2019 EUI: {building.baseline2019EUI || "N/A"}
-              </p>
-              <p className="text-gray-700 text-base">
-                1st Target 2025 EUI: {building.firstTarget2025EUI || "N/A"}
-              </p>
-              <p className="text-gray-700 text-base">
-                2nd Target 2027 EUI: {building.secondTarget2027EUI || "N/A"}
-              </p>
-              <p className="text-gray-700 text-base">
-                Final Target 2030 EUI: {building.finalTarget2030EUI || "N/A"}
-              </p>
-            </li>
+            </div>
           ))}
-        </ul>
+        </div>
       ) : (
         <p>Loading...</p>
       )}
-      <div>
-        <button
-          className="mr-2 border-2 p-2"
-          onClick={prevPage}
-          disabled={currentPage === 1}
-        >
-          Previous Page
-        </button>
-        <button className="border-2 p-2" onClick={nextPage}>
-          Next Page
-        </button>
+      <div className="flex flex-col items-center justify-center pt-2">
+        <div className="flex flex-col items-center justify-center space-y-2 place-items-center">
+          <div>
+            <button
+              onClick={nextPage}
+              className="px-6 py-3 mt-4 text-lg text-white rounded-lg bg-emerald-500 hover:bg-emerald-600"
+            >
+              Show More Buildings
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+              className="p-3 mt-4 text-lg text-white rounded-lg md:px-6 md:py-3 md:mt-4 bg-emerald-500 hover:bg-emerald-600"
+            >
+              Back to Top
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
